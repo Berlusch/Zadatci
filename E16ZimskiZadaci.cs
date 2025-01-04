7f035fc27648ad216b1d4175abe3b2c8cff6ef2c
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-01-04 14:34:33 +0100

    Update E16ZimskiZadaci.cs

--- E16ZimskiZadaci.cs
+++ E16ZimskiZadaci.cs
@@ -33,7 +33,8 @@ namespace Ucenje
                 "Zbroj elemenata niza",
                 "Prosjek ocjena",
                 "Fibonaccijev niz",
-                "Preokret stringa"
+                "Preokret stringa",
+                "Brojanje samoglasnika"
 
             };
 
@@ -83,8 +84,35 @@ namespace Ucenje
                     PreokretStringa();
                     Izbornik();
                     break;
+                case 7:
+                    BrojanjeSamoglasnika();
+                    Izbornik();
+                    break;
             }
         }
+
+        private static void BrojanjeSamoglasnika()
+        {
+            NaslovPrograma("Brojanje samoglasnika");
+            int sum = 0;
+            string[] samoglasnici = ["a", "e", "i", "o", "u"];
+            string upis = E12Metode.UcitajString("Unesite riječ ili rečenicu: ");
+            foreach (char slovo in upis)
+            {
+                if(String.Join("", samoglasnici).Contains(slovo))
+                    {
+                    sum += 1;
+                }
+            }
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("U vašem upisu ima {0} ", sum + " samoglasnika.");
+            Console.ResetColor();
+        }
+
+
+
+
 
         private static void PreokretStringa()
         {
@@ -94,33 +122,32 @@ namespace Ucenje
             {
                 try
                 {
-                    unos = E12Metode.UcitajString("Unesite pojam ili niz znakova (2-20): ");
-                    if (unos.Length<2 || unos.Length > 20)
+                    unos = E12Metode.UcitajString("Unesite pojam ili niz znakova (2-30): ");
+                    if (unos.Length < 2 || unos.Length > 30)
                     {
                         Console.WriteLine("Broj znakova je izvan dozvoljenog raspona, pokušajte ponovno!");
                         continue;
                     }
                     break;
                 }
-                catch 
+                catch
                 {
-                    
-                   
+
+
                 }
-                
+
             }
-            char[] unosZnakovi = new char[unos.Length];
-            for(int i=0; i <unos.Length ; i++)
+            char[] preokret = new char[unos.Length];
+            for (int i = 0; i < unos.Length; i++)
             {
-                unosZnakovi[i] = unos[unos.Length-1-i];
-                
+                preokret[i] = unos[unos.Length - 1 - i];
+
             }
-            Console.WriteLine(string.Join(", ", unosZnakovi));
-
-
-            
-            //Console.WriteLine("Obrnuti redoslijed znakova je {0}", preokret);
-
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Obrnuti redoslijed znakova je ");
+            Console.WriteLine(string.Concat(preokret));
+            Console.ResetColor();
         }
 
         private static void FibonaccijevNiz()
@@ -142,7 +169,7 @@ namespace Ucenje
 
                 catch
                 {
-                    
+
                 }
             }
             int[] niz = new int[n];
@@ -321,89 +348,87 @@ namespace Ucenje
 
         /*
 
-Zimski zadaci:
+    Zimski zadaci:
 
-Jednostavni
-1. Izračun površine pravokutnika:
+    Jednostavni
+    1. Izračun površine pravokutnika:
     Napiši program koji od korisnika traži da unese duljinu i širinu pravokutnika, a zatim izračunava i ispisuje površinu pravokutnika.
-2. Provjera je li broj pozitivan, negativan ili nula:
+    2. Provjera je li broj pozitivan, negativan ili nula:
     Napiši program koji od korisnika traži da unese broj i ispisuje je li broj pozitivan, negativan ili nula.
-3. Zbroj elemenata niza:
+    3. Zbroj elemenata niza:
     Napiši program koji deklarira niz cijelih brojeva, traži od korisnika da unese vrijednosti u niz, a zatim izračunava i ispisuje zbroj svih elemenata niza.
-4. Prosjek ocjena:
+    4. Prosjek ocjena:
     Napiši program koji od korisnika traži da unese broj ocjena, a zatim i same ocjene. Program treba izračunati i ispisati prosjek ocjena.
-5. Ispis Fibonaccijevog niza:
+    5. Ispis Fibonaccijevog niza:
     Napiši program koji od korisnika traži da unese broj n, a zatim ispisuje prvih n brojeva Fibonaccijevog niza (0, 1, 1, 2, 3, 5, 8...).
-6. Preokret stringa:
+    6. Preokret stringa:
     Napiši program koji od korisnika traži da unese string, a zatim ispisuje taj string naopako (npr. "zdravo" postaje "ovardz").
-7. Brojanje samoglasnika:
+    7. Brojanje samoglasnika:
     Napiši program koji od korisnika traži da unese string, a zatim prebrojava i ispisuje koliko samoglasnika ima u tom stringu.
-8. Pretvorba temperature:
+    8. Pretvorba temperature:
     Napiši program koji od korisnika traži da unese temperaturu u Celzijusima i pretvara je u Fahrenheit (°F = °C * 9/5 + 32) ili obrnuto, ovisno o odabiru korisnika.
-9. Sortiranje niza:
+    9. Sortiranje niza:
     Napiši program koji deklarira niz cijelih brojeva, traži od korisnika da unese vrijednosti u niz, a zatim sortira niz uzlazno i ispisuje sortirani niz.
-10. Kalkulator:
+    10. Kalkulator:
     Napiši program koji od korisnika traži da unese dva broja i operaciju (+, -, *, /), a zatim izračunava i ispisuje rezultat. Koristi switch statement za odabir operacije.
 
 
-Generator lozinki
-   Program od korisnika traži unos podataka:
-   Dužina lozinke
-   Uključena/isključena velika slova
-   Uključena/isključena mala slova
-   Uključeni/isključeni brojevi
-   Uključeni/isključeni interpunkcijski znakovi
-   Lozinka počinje ili ne s brojem
-   Lozinka počinje ili ne s interpunkcijskim znakom
-   Lozinka završava ili ne s brojem
-   Lozinka završava ili ne s interpunkcijskim znakom
-   Lozinka ima/nema ponavljajuće znakove
-   Broj lozinki koje je potrebno generirati
+    Generator lozinki
+    Program od korisnika traži unos podataka:
+    Dužina lozinke
+    Uključena/isključena velika slova
+    Uključena/isključena mala slova
+    Uključeni/isključeni brojevi
+    Uključeni/isključeni interpunkcijski znakovi
+    Lozinka počinje ili ne s brojem
+    Lozinka počinje ili ne s interpunkcijskim znakom
+    Lozinka završava ili ne s brojem
+    Lozinka završava ili ne s interpunkcijskim znakom
+    Lozinka ima/nema ponavljajuće znakove
+    Broj lozinki koje je potrebno generirati
 
-   Shodno unesenim pravilima program generira rezultat (jedna ili više lozinki)
-
-
-Ciklična matrica
-Napraviti osnovni zadatak prema inicijalnoj slici
-Dodatno:
-Osigurati unos brojeva redova i kolona u rasponu 2 do 50
-Napraviti da nakon završetka generiranja jedne matrice pita želite li napraviti još jednu i tako sve dok ne unese NE
-Napraviti opcije programa o smjeru kretanja brojeva:
-1. dolje desno početak u smjeru kazaljke na satu (inicijalni zadatak)
-2. dolje lijevo početak u smjeru kazaljke na satu
-3. gore lijevo početak u smjeru kazaljke na satu
-4. gore desno početak u smjeru kazaljke na satu
-
-5. dolje desno početak u kontra smjeru kazaljke na satu 
-6. dolje lijevo početak u kontra smjeru kazaljke na satu
-7. gore lijevo početak u kontra smjeru kazaljke na satu
-8. gore desno početak u kontra smjeru kazaljke na satu
-
-19. sredina (ono što je bio kraj u prvih 8 primjera) lijevo u smjeru kazaljke na satu
-10. sredina (ono što je bio kraj u prvih 8 primjera) desno u smjeru kazaljke na satu
-11. sredina (ono što je bio kraj u prvih 8 primjera) gore u smjeru kazaljke na satu
-12. sredina (ono što je bio kraj u prvih 8 primjera) dolje u smjeru kazaljke na satu
-
-13. sredina (ono što je bio kraj u prvih 8 primjera) lijevo u kontra smjeru kazaljke na satu
-14. sredina (ono što je bio kraj u prvih 8 primjera) desno u kontra smjeru kazaljke na satu
-15. sredina (ono što je bio kraj u prvih 8 primjera) gore u kontra smjeru kazaljke na satu
-16. sredina (ono što je bio kraj u prvih 8 primjera) dolje u kontra smjeru kazaljke na satu
-
-Formatirati brojeve da budu potpisati po pravilima matematike: jedinica ispod jedinice, desetica ispod desetice, stotica ispod stotice
+    Shodno unesenim pravilima program generira rezultat (jedna ili više lozinki)
 
 
+    Ciklična matrica
+    Napraviti osnovni zadatak prema inicijalnoj slici
+    Dodatno:
+    Osigurati unos brojeva redova i kolona u rasponu 2 do 50
+    Napraviti da nakon završetka generiranja jedne matrice pita želite li napraviti još jednu i tako sve dok ne unese NE
+    Napraviti opcije programa o smjeru kretanja brojeva:
+    1. dolje desno početak u smjeru kazaljke na satu (inicijalni zadatak)
+    2. dolje lijevo početak u smjeru kazaljke na satu
+    3. gore lijevo početak u smjeru kazaljke na satu
+    4. gore desno početak u smjeru kazaljke na satu
+
+    5. dolje desno početak u kontra smjeru kazaljke na satu 
+    6. dolje lijevo početak u kontra smjeru kazaljke na satu
+    7. gore lijevo početak u kontra smjeru kazaljke na satu
+    8. gore desno početak u kontra smjeru kazaljke na satu
+
+    19. sredina (ono što je bio kraj u prvih 8 primjera) lijevo u smjeru kazaljke na satu
+    10. sredina (ono što je bio kraj u prvih 8 primjera) desno u smjeru kazaljke na satu
+    11. sredina (ono što je bio kraj u prvih 8 primjera) gore u smjeru kazaljke na satu
+    12. sredina (ono što je bio kraj u prvih 8 primjera) dolje u smjeru kazaljke na satu
+
+    13. sredina (ono što je bio kraj u prvih 8 primjera) lijevo u kontra smjeru kazaljke na satu
+    14. sredina (ono što je bio kraj u prvih 8 primjera) desno u kontra smjeru kazaljke na satu
+    15. sredina (ono što je bio kraj u prvih 8 primjera) gore u kontra smjeru kazaljke na satu
+    16. sredina (ono što je bio kraj u prvih 8 primjera) dolje u kontra smjeru kazaljke na satu
+
+    Formatirati brojeve da budu potpisati po pravilima matematike: jedinica ispod jedinice, desetica ispod desetice, stotica ispod stotice
 
 
 
 
-Ljubavni kalkulator
-Napraviti osnovni zadatak prema inicijalnoj slici koristeći rekurziju
-Dodatno:
-Osigurati unos imena (smije imati samo slova, bez brojeva i interpunkcijskih znakova)
-promijeniti algoritam da zbraja dva susjedna broja (1 i 2, 3 i 4, 5 i 6, itd.) umjesto osnovnog algoritma 1 i zadnji, drugi i predzadnji itd.
-Umjesto rekurzije koristiti petlju po izboru
 
 
+    Ljubavni kalkulator
+    Napraviti osnovni zadatak prema inicijalnoj slici koristeći rekurziju
+    Dodatno:
+    Osigurati unos imena (smije imati samo slova, bez brojeva i interpunkcijskih znakova)
+    promijeniti algoritam da zbraja dva susjedna broja (1 i 2, 3 i 4, 5 i 6, itd.) umjesto osnovnog algoritma 1 i zadnji, drugi i predzadnji itd.
+    Umjesto rekurzije koristiti petlju po izboru
 
 
 
@@ -411,14 +436,16 @@ Umjesto rekurzije koristiti petlju po izboru
 
 
 
-Kada završite sve zadatke s svim opcijama javite se na email pa ću poslati nove :)
+
+
+    Kada završite sve zadatke s svim opcijama javite se na email pa ću poslati nove :)
 
 
 
 
 
 
-*/
+    */
 
 
 
