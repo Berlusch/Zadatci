7f547bfaed6c8f82cd759026724090f6adf69733
Bernarda Lusch <bernarda.lusch@gmail.com>
2025-01-08 16:11:23 +0100

    Rekurzija

--- E10LjubavniKalkulator.cs
+++ E10LjubavniKalkulator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -25,53 +26,72 @@ namespace Ucenje
             string ime1 = E12Metode.UcitajString("Unesi svoje ime: ").ToUpper();
             string ime2 = E12Metode.UcitajString("Unesi ime svoje simpatije: ").ToUpper();
 
-            {
-                int rezultat = LjubavnaKalkulacija(ime1, ime2);
-                Console.WriteLine("{0} i {1} imaju ljubavni rezultat {2}%", ime1, ime2, rezultat);
-            }
-
-        }
-        private static int LjubavnaKalkulacija(string ime1, string ime2)
-        {
             char[] imena = (ime1 + ime2).ToCharArray();
 
-            Dictionary<char, int> imenabrojevi = new Dictionary<char, int>();
+            Dictionary<char, int> brojac = new Dictionary<char, int>();
 
             foreach (char slovo in imena)
             {
-                if (imenabrojevi.ContainsKey(slovo))
+                if (brojac.ContainsKey(slovo))
                 {
-                    imenabrojevi[slovo]++;
+                    brojac[slovo]++;
                 }
                 else
                 {
-                    imenabrojevi[slovo] = 1;
+                    brojac[slovo] = 1;
                 }
             }
-            int[] brojPonavljanja = new int[imena.Length]; 
+            int[] imenabrojevi = new int[imena.Length];
 
-            
+
             for (int i = 0; i < imena.Length; i++)
             {
-                brojPonavljanja[i] = imenabrojevi[imena[i]]; 
+                imenabrojevi[i] = brojac[imena[i]];
             }
 
-            return 0;
+            string rezultat = ZbrojiZnamenke(imenabrojevi);
+            if (int.Parse(rezultat) <= 25)
+                Console.WriteLine("{0} i {1} imaju ljubavni rezultat {2}%. Nažalost, vi niste jedno za drugo :(", ime1, ime2, rezultat);
+            else if (int.Parse(rezultat) > 25 && int.Parse(rezultat) <= 50)
+                Console.WriteLine("{0} i {1} imaju ljubavni rezultat {2}%. Hm, možda ipak možeš naći nekog boljeg?", ime1, ime2, rezultat);
+            else if (int.Parse(rezultat) > 50 && int.Parse(rezultat) <= 75)
+                Console.WriteLine("{0} i {1} imaju ljubavni rezultat {2}%. Ova kombinacija ima potencijala, samo naprijed!", ime1, ime2, rezultat);
+            else
+                Console.WriteLine("{0} i {1} imaju ljubavni rezultat {2}%. Pa ovo je prava ljubav, čestitamo! <3", ime1, ime2, rezultat);
+
 
         }
 
-
-        /*private static int Zbroji(int broj)
+        private static string ZbrojiZnamenke(int[] broj)
         {
-            if (broj == 1)
+            //ako je broj znamenki 0-2, vraća taj broj
+            if (broj.Length <= 1)
             {
-                return 1;
+                return string.Join("", broj);
             }
-            return broj + Zbroji(broj - 1);
+            int prvi = broj[0];
+            int zadnji = broj[broj.Length-1];
+            int sum = prvi + zadnji;
+
+            int[] sredina = new int[broj.Length - 2];
+            Array.Copy(broj, 1, sredina, 0, broj.Length - 2);
+            string rezultatSredine = ZbrojiZnamenke(sredina);
+            string rezultat = sum.ToString() + rezultatSredine;
+            if (rezultat.Length > 2)
+            {
+                return ZbrojiZnamenke(stringUBroj(rezultat));
+            }
+            return rezultat;
         }
-        //100 + 99 + ...+1*/
 
-
-
+        private static int[] stringUBroj(string broj)
+        {
+            int[] brojevi = new int[broj.Length];
+            for (int i = 0; i < broj.Length; i++)
+            {
+                brojevi[i] = int.Parse(broj[i].ToString());  // Pretvaranje char u int može i ovako: - '0'(ASCII kod)
+            }
+            return brojevi;
+        }
     }
 }
